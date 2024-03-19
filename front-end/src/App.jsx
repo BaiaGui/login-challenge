32b05aa0f72ae28022976ca22fd222e20bceaa72
Guilherme Baia <guilhermebaiav@gmail.com>
2024-03-19 18:38:11 -0300

    create sign up layout

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { LoginPage } from './LoginPage'
+import { SignUpPage } from './SignUpPage'
 
 function App() {
 
   return (
-    <LoginPage/>
+    <SignUpPage/>
   )
 }
 
