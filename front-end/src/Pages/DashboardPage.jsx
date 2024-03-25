63ef5d1a08edc5757dec1d7e9f73c933fea14a63
Guilherme Baia <guilhermebaiav@gmail.com>
2024-03-25 20:34:22 -0300

    add shadcn ui

---
+++ front-end/src/Pages/DashboardPage.jsx
@@ -0,0 +1,27 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "../Components/ui/navigation-menu";
+
+export function DashboardPage() {
+  return (
+    <main>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </main>
+  );
+}
