a8dd6382f8fb17ac123287ce15e4cb0d9b50b604
Guilherme Baia <guilhermebaiav@gmail.com>
2024-03-25 16:31:53 -0300

    connect register endpoint

--- front-end/src/Pages/LoginPage.jsx
+++ front-end/src/Pages/LoginPage.jsx
@@ -4,12 +4,22 @@ import Icon from "../assets/ConsulTIIcone.png"
 import { Input } from "../Components/Input";
 
 export function LoginPage(){
+
+    function handleSubmit(e){
+
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson)
+    }
+
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center">
             <nav className="w-full h-fit px-48 py-5 absolute top-0"><img src={Icon} alt="ConsulTI Logo" width="50px"/></nav>
-            <form action="" className="flex flex-col items-center px-10 py-8 w-96 gap-3">
+            <form onSubmit={handleSubmit} className="flex flex-col items-center px-10 py-8 w-96 gap-3">
                 <h1 className="text-5xl font-bold mb-10">Fazer Login</h1>
-                <Input name="Email" type="email"/>
+                <Input name="Email" type="email" isFirst={true}/>
                 <Input name="Senha" type="password"/>
                 <Submit>Entrar</Submit>
                 <Link to="/signUp" className="text-sm mt-20 text-stone-500 hover:underline outline-none focus:ring-2">Não possui uma conta? Cadastre-se</Link>
--- front-end/src/Pages/SignUpPage.jsx
+++ front-end/src/Pages/SignUpPage.jsx
@@ -1,36 +1,51 @@
 import { Link } from "react-router-dom";
 import { Submit } from "../Components/Submit";
-import Icon from "../assets/ConsulTIIcone.png"
+import Icon from "../assets/ConsulTIIcone.png";
 import { Input } from "../Components/Input";
+import { useEffect, useState } from "react";
 
-export function SignUpPage(){
+export function SignUpPage() {
+  const [userData, setUserData] = useState();
 
-    function handleSubmit(e){
-        e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-        // console.log(formJson);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    setUserData(formJson);
+  }
+
+  useEffect(() => {
+    async function registerUser() {
+      const response = await fetch("https://login-challenge-back-end.onrender.com/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
     }
+    registerUser();
+  }, [userData]);
 
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <nav className="w-full h-fit px-48 py-5 absolute top-0">
+        <img src={Icon} alt="ConsulTI Logo" width="50px" />
+      </nav>
 
-
-
-
-
-    return (
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-            <nav className="w-full h-fit px-48 py-5 absolute top-0"><img src={Icon} alt="ConsulTI Logo" width="50px"/></nav>
-           
-            <form onSubmit={handleSubmit} className="flex flex-col items-center px-10 py-8 w-fit gap-3">
-                <h1 className="text-5xl font-bold mb-10">Fazer Cadastro</h1>
-                <Input name="Usuário" type="text"/>
-                <Input name="Email" type="email"/>
-                <Input name="Senha" type="password"/>
-                <Submit>Enviar</Submit>
-                <Link to="/" className="text-sm mt-20 text-stone-500 hover:underline outline-none focus:ring-2">Já possui uma conta? Fazer login</Link>
-            </form>
-        </div>
-    );
+      <form onSubmit={handleSubmit} className="flex flex-col items-center px-10 py-8 w-fit gap-3">
+        <h1 className="text-5xl font-bold mb-10">Fazer Cadastro</h1>
+        <Input placeholder="Usuário" name="username" type="text" isFirst={true} />
+        <Input placeholder="Email" name="email" type="email" />
+        <Input placeholder="Senha" name="password" type="password" />
+        <Submit>Enviar</Submit>
+        <Link to="/" className="text-sm mt-20 text-stone-500 hover:underline outline-none focus:ring-2">
+          Já possui uma conta? Fazer login
+        </Link>
+      </form>
+    </div>
+  );
 }
-
