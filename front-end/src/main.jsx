6c8651ee0a0fea769d7f559d2430b3e30d947019
Guilherme Baia <guilhermebaiav@gmail.com>
2024-03-25 21:14:02 -0300

    add dashboard page

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { SignUpPage } from './Pages/SignUpPage.jsx';
-import { LoginPage } from './Pages/LoginPage.jsx';
+import { SignUpPage } from "./Pages/SignUpPage.jsx";
+import { LoginPage } from "./Pages/LoginPage.jsx";
+import { DashboardPage } from "./Pages/DashboardPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginPage/>,
+    element: <LoginPage />,
   },
   {
     path: "/signUp",
-    element: <SignUpPage/>
+    element: <SignUpPage />,
   },
-])
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
+  },
+]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
